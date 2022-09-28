c2d2655c2dcbbee19fa32346e887da96df1f1239
Adam Robson <adamray312@gmail.com>
2022-09-27 23:39:41 -0700

    final css

--- src/Components/Colors/Colors.js
+++ src/Components/Colors/Colors.js
@@ -5,7 +5,7 @@ export default function Colors() {
   return (
     <section className="main">
       <div className="colorsContainer">
-        <NavLink className="champagne" to="/color/f1/e0/c5" style={ { color: '#ffb563' } }>champage</NavLink>
+        <NavLink className="champagne" to="/color/f1/e0/c5" style={ { color: '#f1e0c5' } }>champage</NavLink>
         <NavLink className="mauve" to="/color/ed/c9/ff" style={ { color: '#edc9ff' } }>mauve</NavLink>
         <NavLink className="xanadu" to="/color/71/81/6d" style={ { color: '#71816d' } }>xanadu</NavLink>
         <NavLink className="midnight-green-eagle-green" to="/color/0b/3c/49" style={ { color: '#0b3c49' } }>midnight green<br /> eagle green</NavLink>
