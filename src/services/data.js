2d841a538d58868cb8f8540b444dd6b1ed8eac41
Adam Robson <adamray312@gmail.com>
2022-10-12 13:18:38 -0700

    new data file

--- src/services/data.js
+++ src/services/data.js
@@ -1,43 +1,22 @@
-//eslint-disable-next-line
-// export default colorlist = [
-//   {
-//     id: 'CFDEE7',
-//     title: 'beau blue',
-//     type: 'hex',
-//     prefix: 'cf',
-//     body: 'de',
-//     suffix: 'e7',
-//   },
-//   {
-//     id: 'FFD29D',
-//     title: 'deep champagne',
-//     type: 'hex',
-//     prefix: 'ff',
-//     body: 'd2',
-//     suffix: '9d',
-//   },
-//   {
-//     id: 'FFB563',
-//     title: 'rajah',
-//     type: 'hex',
-//     prefix: 'ff',
-//     body: 'b5',
-//     suffix: '63',
-//   },
-//   {
-//     id: '087e8b',
-//     title: 'Metallic Seaweed',
-//     type: 'hex',
-//     prefix: '08',
-//     body: '7e',
-//     suffix: '8b',
-//   },
-//   {
-//     id: '5e565a',
-//     title: 'dark liver',
-//     type: 'hex',
-//     prefix: '5e',
-//     body: '56',
-//     suffix: '5a',
-//   }
-// ];
+export const colors = [
+  {
+    color: '#f1e0c5',
+    title: 'champagne'
+  },
+  {
+    color: '#edc9ff',
+    title: 'mauve',
+  },
+  {
+    color: '#71816d',
+    title: 'xanadu'
+  },
+  {
+    color: '#0b3c49',
+    title: 'midnight green eagle green'
+  },
+  {
+    color: '#333745',
+    title: 'gunmetal'
+  }
+];
